f02ec451afa5a5ad2f2d46acb6e8c8bd93db8f05
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-03-08 08:39:54 +0200

    add assign variable to html elements

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputEl = document.getElementById("input");
+const infoTextEl = document.getElementById("info-text");
+const meaningContainerEl = document.getElementById("meaning-container");
+const titleEl = document.getElementById("title");
+const meaningEl = document.getElementById("meaning");
+console.log(meaningEl);
