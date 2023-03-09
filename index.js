db91c2384ac6deebb172205df07dcb29c1205743
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-03-09 10:42:36 +0200

    dictionary functionaliy

--- index.js
+++ index.js
@@ -3,4 +3,43 @@ const infoTextEl = document.getElementById("info-text");
 const meaningContainerEl = document.getElementById("meaning-container");
 const titleEl = document.getElementById("title");
 const meaningEl = document.getElementById("meaning");
-console.log(meaningEl);
+const audioEl = document.getElementById("audio");
+
+async function fetchAPI() {
+  try {
+    //display before fetching
+    infoTextEl.style.display = "block";
+    meaningContainerEl.style.display = "none";
+    infoTextEl.innerText = `Searching for the word ${word}`;
+
+    const apiURL = "https://api.dictionaryapi.dev/api/v2/entries/en/<word>";
+    const result = await fetch(apiURL);
+
+    if (result.title) {
+      meaningContainerEl.style.display = "block";
+      infoTextEl.style.dispay = "none";
+      titleEl.innerText = word;
+      meaningEl.innerText = "N/A";
+      audioEl.style.display = "none";
+    } else {
+      infoTextEl.style.display = "none";
+      meaningContainerEl.style.display = "block";
+      audioEl.style.display = "inline-flex";
+      titleEl.innerText = result[0].word;
+      meaningEl.innerText = result[0].definitions[0].definition;
+      audioEl.src = result[0].phonetics[0].audio;
+    }
+  } catch (error) {
+    console.log(error);
+    infoTextEl.innerText = "An error happen, try again later";
+  }
+}
+
+inputEl.addEventListener("keyup", (e) => {
+  console.log(e);
+  console.log(e.target.value);
+
+  if (e.target.value && e.key === "Enter") {
+    fetchAPI(e.target.value);
+  }
+});
