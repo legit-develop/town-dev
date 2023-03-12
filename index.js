88e5c2961893476fc16e624954950c6a0af62df7
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-03-12 11:14:56 +0200

    create html variables

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const btnEl = document.getElementById("btn");
-const animeContainerEl = document.getElementById("anime-container");
-const animeImgEl = document.getElementById("anime-img");
-const animeNameEl = document.getElementById("anime-name");
-console.log(animeNameEl);
+const celsiusEl = document.getElementById("celsius");
+const fahrenheit = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
+
+function computeTemp(event) {
+  console.log(event.target.name);
+  console.log(event.target.value);
+}
