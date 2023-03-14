a4c792d3a4d603bf49a469d17609fe9afbebe653
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-03-14 16:14:11 +0200

    assign variables to html elements

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-const inputEl = document.getElementById("input");
-const errorEl = document.getElementById("error");
-const resultEl = document.getElementById("result");
-
-let errorTime;
-let resultTime;
-
-function updateResult() {
-  if (inputEl.value <= 0 || isNaN(inputEl.value)) {
-    errorEl.innerText = `${inputEl.value} is invalid input`;
-    clearTimeout(errorTime);
-    errorTime = setTimeout(() => {
-      errorEl.innerText = " ";
-      inputEl.value = "";
-    }, 2000);
-  } else {
-    resultEl.innerText = (+inputEl.value / 2.2).toFixed(1);
-    clearTimeout(resultTime);
-    resultTime = setTimeout(() => {
-      resultEl.innerText = "";
-      inputEl.value = "";
-    }, 1000);
-  }
-}
-
-inputEl.addEventListener("input", updateResult);
+//html elements
+const loanAmountValue = document.getElementById("loan-amount");
+const interestRateValue = document.getElementById("interest-rate");
+const monthsToPayValue = document.getElementById("months-to-pay");
