74c4820e95b2f71223f3f2371895cf6e4c926352
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-03-15 13:13:59 +0200

    add functionality of loan calculator

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 //html elements
+calculateLoan();
 const loanAmountValue = document.getElementById("loan-amount");
 const interestRateValue = document.getElementById("interest-rate");
 const monthsToPayValue = document.getElementById("months-to-pay");
+const monthPay = document.getElementById("payment");
+const totalAmount = document.getElementById("total-amount");
+const bankProfitEl = document.getElementById("bank-profit");
+
+function calculateLoan() {
+  interest = (loanAmountValue * (interestRateValue * 0.01)) / monthsToPayValue;
+  monthlyPayment = (loanAmountValue / monthsToPayValue + interest).toFixed(0);
+  monthPay.innerText = `Monthly Payment: ${monthlyPayment}`;
+  totalAmountPayed = (monthlyPayment * monthsToPayValue).toFixed(0);
+  totalAmount.innerText = `Total Amount payed: R ${totalAmountPayed}`;
+  bankMoney = (totalAmountPayed - loanAmountValue).toFixed(0);
+  bankProfitEl.innerText = `Bank Profit R ${bankMoney}`;
+}
