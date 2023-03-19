cfd3e7e6f8e0d15bba7e1a718507942c1e82eba1
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-03-19 10:56:32 +0200

    add toggle button functionality

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-//html elements
-calculateLoan();
-const loanAmountValue = document.getElementById("loan-amount");
-const interestRateValue = document.getElementById("interest-rate");
-const monthsToPayValue = document.getElementById("months-to-pay");
-const monthPay = document.getElementById("payment");
-const totalAmount = document.getElementById("total-amount");
-const bankProfitEl = document.getElementById("bank-profit");
+const inputEl = document.querySelector(".input");
+const bodyEl = document.querySelector("body");
 
-function calculateLoan() {
-  interest = (loanAmountValue * (interestRateValue * 0.01)) / monthsToPayValue;
-  monthlyPayment = (loanAmountValue / monthsToPayValue + interest).toFixed(0);
-  monthPay.innerText = `Monthly Payment: ${monthlyPayment}`;
-  totalAmountPayed = (monthlyPayment * monthsToPayValue).toFixed(0);
-  totalAmount.innerText = `Total Amount payed: R ${totalAmountPayed}`;
-  bankMoney = (totalAmountPayed - loanAmountValue).toFixed(0);
-  bankProfitEl.innerText = `Bank Profit R ${bankMoney}`;
+inputEl.checked = false;
+
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "#000";
+  } else {
+    bodyEl.style.background = "#fff";
+  }
 }
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
