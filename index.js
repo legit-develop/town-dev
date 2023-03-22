976672c11d928a0e2a4e752d8de5ea2e02676865
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-03-22 15:23:57 +0200

    set html element

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const inputEl = document.querySelector(".input");
-const bodyEl = document.querySelector("body");
-
-inputEl.checked = false;
-
-updateBody();
-
-function updateBody() {
-  if (inputEl.checked) {
-    bodyEl.style.background = "#000";
-  } else {
-    bodyEl.style.background = "#fff";
-  }
-}
-
-inputEl.addEventListener("input", () => {
-  updateBody();
-  updateLocalStorage();
-});
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
