3434a882ea81be83653e4e1b7da57a799ab722da
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-04-02 17:59:07 +0200

    html elements

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-const hourEl = document.querySelector(".hour");
-const minuteEl = document.querySelector(".minute");
-const secondEl = document.querySelector(".second");
-
-function updateClock() {
-  const currentDate = new Date();
-  setTimeout(updateClock, 1000);
-
-  const hour = currentDate.getHours();
-  const minutes = currentDate.getMinutes();
-  const seconds = currentDate.getSeconds();
-  const hourDeg = (hour / 12) * 360;
-  hourEl.style.transform = `rotate(${hourDeg}deg)`;
-  const minuteDeg = (minutes / 60) * 360;
-  minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
-  const secondDeg = seconds / 60;
-  secondEl.style.transform = `rotate(${secondDeg}deg)`;
-}
-
-updateClock();
+const navbarEl = document.querySelector(".navbar");
+const bottomContainerEl = document.querySelector(".bottom-container");
